01708b4bea7db73c35a3ce72b27a4beaf532c5be
pettitkd <pettitkd@gmail.com>
2024-11-06 17:07:03 -0800

    Fixed assignment to move_bit (again).

--- src/project.v
+++ src/project.v
@@ -23,7 +23,7 @@ module tt_um_7seg_snake
   reg         dp;
   reg         dir;      // 0 = clockwise, 1 = counter clockwise
   wire        move;     // 1'b1 when time for the snake to move 
-  wire        move_bit;
+  reg         move_bit;
   reg         move_p1;
   reg         move_p2;
   reg [25:0]  move_count; // Counter to tell when to move the snake
@@ -126,24 +126,24 @@ module tt_um_7seg_snake
 
   always @*
   begin
-    move_bit = 1'b0;
+    move_bit <= 1'b0;
     case (ui_in[3:0])
-    4'h0:  move_bit = move_count[25];
-    4'h1:  move_bit = move_count[24];
-    4'h2:  move_bit = move_count[23];
-    4'h3:  move_bit = move_count[22];
-    4'h4:  move_bit = move_count[21];
-    4'h5:  move_bit = move_count[20];
-    4'h6:  move_bit = move_count[19];
-    4'h7:  move_bit = move_count[18];
-    4'h8:  move_bit = move_count[17];
-    4'h9:  move_bit = move_count[16];
-    4'ha:  move_bit = move_count[15];
-    4'hb:  move_bit = move_count[14];
-    4'hc:  move_bit = move_count[13];
-    4'hd:  move_bit = move_count[12];
-    4'he:  move_bit = move_count[11];
-    4'hf:  move_bit = move_count[10];
+    4'h0:  move_bit <= move_count[25];
+    4'h1:  move_bit <= move_count[24];
+    4'h2:  move_bit <= move_count[23];
+    4'h3:  move_bit <= move_count[22];
+    4'h4:  move_bit <= move_count[21];
+    4'h5:  move_bit <= move_count[20];
+    4'h6:  move_bit <= move_count[19];
+    4'h7:  move_bit <= move_count[18];
+    4'h8:  move_bit <= move_count[17];
+    4'h9:  move_bit <= move_count[16];
+    4'ha:  move_bit <= move_count[15];
+    4'hb:  move_bit <= move_count[14];
+    4'hc:  move_bit <= move_count[13];
+    4'hd:  move_bit <= move_count[12];
+    4'he:  move_bit <= move_count[11];
+    4'hf:  move_bit <= move_count[10];
     endcase
   end
 
